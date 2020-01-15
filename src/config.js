7b95249cd7cb29e96cac409239f90289e6eb0a74
loditzz <loditzz@gmail.com>
2020-01-15 20:52:02 -0300

    Primeiros componentes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "sa-east-1",
+    BUCKET: "first-app-ditzz"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://crsjw3l5w4.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_DF9gCTmlk",
+    APP_CLIENT_ID: "3mp0vunbjfmeh37glhsimjuc12",
+    IDENTITY_POOL_ID: "us-east-2:8bbdad67-5d43-4692-9b62-0359923796d2"
+  }
+};
